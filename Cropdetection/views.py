66e1b8c18bc37daf9be95d249f120bc6237782ed
kimathi31 <99385960+kimathi31@users.noreply.github.com>
2022-04-06 15:17:13 +0300

    Add files via upload

--- Cropdetection/views.py
+++ Cropdetection/views.py
@@ -3,50 +3,87 @@ from PIL import Image as im
 import torch
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
-from .db import crop_analysis
+from .db import crop_analysis,CropIdentification
 from .forms import ImageUploadForm
+import datetime
+import base64
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+
+class_labels ={ 0 : 'jute', 1 : 'maize', 2 : 'rice', 3 : 'sugarcane', 4 : 'tomato', 5 : 'wheat'}
+tfmodel = tf.keras.models.load_model("C:/Users/bkimathi/app/Cropdetection/Trained Models/2022-03-02-12_07-cropdetectionmodel.h5")
+IMG_SIZE = 224
+
+def getcrop(img):
+    # turn image into numerical tensors
+    image = tf.image.decode_jpeg(img, channels=3)
+    # covert colour channel values
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    # resize image
+    image = tf.image.resize(image, size=[IMG_SIZE, IMG_SIZE])
+    image = np.expand_dims(image, axis=0)
+    pred = tfmodel.predict(image)
+    pred_crop = class_labels[np.argmax(pred)]
+    return pred_crop
+
 
 def post(request):
     form = ImageUploadForm(request.POST, request.FILES)
     if form.is_valid():
-        img = request.FILES.get('image')
+        image = request.FILES.get('image')
         
-        uploaded_img_qs = img
-        img_bytes = uploaded_img_qs
-        img = im.open(img_bytes)
+        img = image.file.read()
+        
+        #decode image from bytes
+        encoded_img = im.open(image)       
 
-        # Change this to the correct path
-        path_hubconfig = "C:/Users/bkimathi/Desktop/app/Cropdetection/yolov5"
-        path_weightfile = "C:/Users/bkimathi/Desktop/app/Cropdetection/Trained Models/last.pt"  
-        model = torch.hub.load(path_hubconfig, 'custom',
+
+        pred_crop = getcrop(img)
+
+
+        if pred_crop == 'tomato':
+            path_hubconfig = "C:/Users/bkimathi/app/Cropdetection/yolov5"
+            path_weightfile = "C:/Users/bkimathi/app/Cropdetection/Trained Models/last.pt"  
+            
+            
+            model = torch.hub.load(path_hubconfig, 'custom',
                                path=path_weightfile, source='local')
+            results = model(encoded_img, size=128)
+            results.render()
+            for img in results.imgs:
+                img_base64 = im.fromarray(img)
+                img_base64.save("media/yolo_out", format="JPEG")
 
-        results = model(img, size=128)
-        results.render()
-        for img in results.imgs:
-            img_base64 = im.fromarray(img)
-            img_base64.save("media/yolo_out", format="JPEG")
-
-        inference_img = "/media/yolo_out"
-
-        form = ImageUploadForm()
-        context = {
+            inference_img = "/media/yolo_out"
+            post = crop_analysis()
+            post.Analysis_REF_No 
+            post.Crop = pred_crop
+            #post.Status = Status
+            post.Date_created = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            #post.Image = inference_img
+            post.save()
+            form = ImageUploadForm()
+            context = {
                 "form": form,
                 "inference_img": inference_img
             }
-        return render(request, 'Cropdetection/Analysis.html', context)
-
+    
+        else:
+            message = print('Crop is not in detection Class')
+            context = {"message": message}
+    
     else:
         form = ImageUploadForm()
         context = {
             "form": form
         }
-        return render(request, 'Cropdetection/Analysis.html', context)
-        
+    return render(request, 'Cropdetection/Analysis.html', context)    
+ 
         
 def report_table(request):
     analysis_table = crop_analysis.objects.all()
-    context = {'analysis_table':analysis_table}
+    context = {'analysis_table': analysis_table}
 
     return render(request, 'Cropdetection/data.html', context)
         
